cbb1cf8127909dce831d9ae6bc8f3ff42211ea3a
ebrentinambunan <ebrentinambunan2003@gmail.com>
2025-11-30 02:53:16 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+/* ---------------------------
+          NAVIGASI SECTION
+    ----------------------------- */
+    function navigate(id, btn) {
+      document.querySelectorAll('main section').forEach(section => {
+        section.classList.remove('active');
+      });
+      document.getElementById(id).classList.add('active');
+
+      document.querySelectorAll('nav.bottom-nav button').forEach(button => {
+        button.classList.remove('active');
+      });
+      btn.classList.add('active');
+    }
+
+
+    /* ---------------------------
+          TYPING EFFECT JS
+    ----------------------------- */
+    const typingElement = document.getElementById("typing-text");
+    const cursor = document.getElementById("cursor");
+
+    const phrases = [
+      "Saya Seorang Mahasiswa Teknik Informatika...",
+      "dan juga Seorang FullStack Developer."
+    ];
+
+    let phraseIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    let delay = 500;
+
+    function type() {
+      const currentPhrase = phrases[phraseIndex];
+      const currentText = currentPhrase.substring(0, charIndex);
+
+      typingElement.textContent = currentText;
+
+      if (!isDeleting) {
+        if (charIndex < currentPhrase.length) {
+          charIndex++;
+        } else {
+          isDeleting = true;
+          delay = 1200;
+        }
+      } else {
+        if (charIndex > 0) {
+          charIndex--;
+        } else {
+          isDeleting = false;
+          phraseIndex = (phraseIndex + 1) % phrases.length;
+          delay = 300;
+        }
+      }
+
+      setTimeout(type, isDeleting ? 50 : delay);
+    }
+
+    function blinkCursor() {
+      cursor.style.opacity = cursor.style.opacity === "0" ? "1" : "0";
+    }
+
+    type();
+    setInterval(blinkCursor, 500);
